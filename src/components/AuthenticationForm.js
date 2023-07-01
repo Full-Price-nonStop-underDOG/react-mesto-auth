ec885c9cde6a56a8db3a152e43a808b832a0c333
full-price-nonstop-underdog <korvin.korp@yandex.ru>
2023-07-01 11:02:46 +0300

    thats zeds

--- src/components/AuthenticationForm.js
+++ src/components/AuthenticationForm.js
@@ -19,45 +19,47 @@ function AuthenticationForm({ onSubmit, title, buttonText }) {
   }
 
   return (
-    <section className="authentication">
-      <h2 className="authentication__title">{title}</h2>
-      <form className="authentication__form" onSubmit={handleSubmit}>
-        <input
-          className="authentication__form-input"
-          placeholder="Email"
-          name="email"
-          type="email"
-          required
-          value={email || ""}
-          onChange={handleEmailChange}
-        />
-        <input
-          className="authentication__form-input"
-          placeholder="Пароль"
-          name="password"
-          type="password"
-          required
-          value={password || ""}
-          onChange={handlePasswordChange}
-        />
+    <div className="page">
+      <section className="authentication">
+        <h2 className="authentication__title">{title}</h2>
+        <form className="authentication__form" onSubmit={handleSubmit}>
+          <input
+            className="authentication__form-input"
+            placeholder="Email"
+            name="email"
+            type="email"
+            required
+            value={email || ""}
+            onChange={handleEmailChange}
+          />
+          <input
+            className="authentication__form-input"
+            placeholder="Пароль"
+            name="password"
+            type="password"
+            required
+            value={password || ""}
+            onChange={handlePasswordChange}
+          />
 
-        <button className="authentication__form-submit" type="submit">
-          {buttonText}
-        </button>
+          <button className="authentication__form-submit" type="submit">
+            {buttonText}
+          </button>
 
-        <div className="authentication__sign-up">
-          <p className="authentication__sign-up_words">
-            {title === "Регистрация" ? "Уже зарегистрированы?" : ""}
-          </p>
-          <Link
-            to={title === "Регистрация" ? "" : ""}
-            className="authentication__sign-up_words"
-          >
-            {title === "Регистрация" ? "Войти" : ""}
-          </Link>
-        </div>
-      </form>
-    </section>
+          <div className="authentication__sign-up">
+            <p className="authentication__sign-up_words">
+              {title === "Регистрация" ? "Уже зарегистрированы?" : ""}
+            </p>
+            <Link
+              to={title === "Регистрация" ? "" : ""}
+              className="authentication__sign-up_words"
+            >
+              {title === "Регистрация" ? "Войти" : ""}
+            </Link>
+          </div>
+        </form>
+      </section>
+    </div>
   );
 }
 
